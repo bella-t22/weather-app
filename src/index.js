08f974ca80a4f3925b4b015b05f3f37ca57a3344
bella-t22 <b.jtorr22@gmail.com>
2024-01-06 21:13:43 -0800

    Finish functionality for searching a location

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const precip = document.querySelector('#precip');
 const humidity = document.querySelector('#humidity');
 const uv = document.querySelector('#uv');
 const lastUpdated = document.querySelector('#last-updated');
+const btn = document.querySelector('button');
 
 const defaultLocation = async () => {
     const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=17a1fc3fa922438dbbe01411211711&q=London&days=3&aqi=yes&alerts=no', {mode: 'cors'});
@@ -30,23 +31,33 @@ const defaultLocation = async () => {
 
 defaultLocation();
 
-const searchLocation = async (userInput) => {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=17a1fc3fa922438dbbe01411211711&q=${userInput}&days=3&aqi=yes&alerts=no`, {mode: 'cors'});
-    const data = await response.json();
-    console.log(data);
-    console.log('location:', data.location.name, data.location.region, data.location.country);
-    console.log('temp:', data.current.temp_f);
-    console.log('condition:', data.current.condition.text);
-    console.log('icon:', data.current.condition.icon)
-    console.log('feels like:', data.current.feelslike_f);
-    console.log('wind:', data.current.wind_mph, data.current.wind_dir);
-    console.log('precip:', data.current.precip_in);
-    console.log('humidity:', data.current.humidity);
-    console.log('uv index:', data.current.uv);
-    console.log('last updated:', data.current.last_updated);
+const searchLocation = async (value) => {
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=17a1fc3fa922438dbbe01411211711&q=${value}&days=3&aqi=yes&alerts=no`, {mode: 'cors'});
+        const data = await response.json();
+        location.textContent = `${data.location.name},`;
+        extendLocation.textContent = `${data.location.region}, ${data.location.country}`;
+        temp.textContent = data.current.temp_f;
+        condition.textContent = data.current.condition.text;
+        icon.src = data.current.condition.icon;
+        feelsLike.textContent = data.current.feelslike_f;
+        wind.textContent = `${data.current.wind_mph}, ${data.current.wind_dir}`;
+        precip.textContent = data.current.precip_in;
+        humidity.textContent = data.current.humidity;
+        uv.textContent = data.current.uv;
+        lastUpdated.textContent = data.current.last_updated;
+    } catch {
+        alert('Oops! That does not work. Try again!');
+    }
 }
 
-const switchToCelsius = (data) => {
-    console.log('temp:', data.current.temp_c);
-    console.log('feels like:', data.current.feelslike_c);
-}
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const userInput = document.querySelector('input');
+    searchLocation(userInput.value);
+})
+
+// const switchToCelsius = (data) => {
+//     console.log('temp:', data.current.temp_c);
+//     console.log('feels like:', data.current.feelslike_c);
+// }
